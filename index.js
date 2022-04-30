fc280637deef6b31bcce7ed696dcd75a048219ec
justdpine <93888647+justdpine@users.noreply.github.com>
2022-04-30 11:59:00 -0700

    setting up project for module importing

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-var sliderPicker = new iro.ColorPicker("#sliderPicker", {
+import { dragElement } from './modules/utilities.js'
+
+const dragElementObj = document.getElementById("drag-object")
+const hexInput = document.getElementById('hexInput');
+const colorView = document.querySelector('.color-view');
+const sliderPicker = new iro.ColorPicker("#sliderPicker", {
     width: 200,
     color: "rgb(255, 0, 0)",
     borderWidth: 1,
@@ -24,73 +29,23 @@ var sliderPicker = new iro.ColorPicker("#sliderPicker", {
       },
       ]
     });
-  //     {
-  //       // component: iro.ui.Slider,
-  // //       options: {
-  // //         sliderType: 'alpha'
-  // //       }
-  // //     },
-  // //   ]
-  // // });
-  var hexInput = document.getElementById('hexInput');
-  var colorView = document.querySelector('.color-view');
-  const dragElementObj = document.getElementById("drag-object")
-  sliderPicker.on(['color:init', 'color:change'], function(color){
-    const { hexString } = color
-    hexInput.value = hexString;
-    colorView.style.backgroundColor = hexString;
-    dragElementObj.style.backgroundColor = hexString;
-  });
-  
-  hexInput.addEventListener('change', function() {
-    const colorValue = this.value;
-    console.log(colorValue);
-    sliderPicker.color.hexString = this.value;
-  });
-  
+
+sliderPicker.on(['color:init', 'color:change'], function(color){
+  const { hexString } = color
+  hexInput.value = hexString;
+  colorView.style.backgroundColor = hexString;
+  dragElementObj.style.backgroundColor = hexString;
+});
+
+hexInput.addEventListener('change', function() {
+  const colorValue = this.value;
+  console.log(colorValue);
+  sliderPicker.color.hexString = this.value;
+});
+
 // drag and drop script
 
 dragElement(dragElementObj);
-
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // this I find the cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the user moves their cursor:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // finds the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    // "places" the element after the mouse stops/release:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
   
   
   
