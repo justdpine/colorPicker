d8253f91e6b8a18a4858660cc2011125cee6612a
Wendy <misswendymoira@gmail.com>
2022-05-03 20:09:00 -0700

    removed duplicate color card
    prepping for merge. removed D's code, as theirs now has iterations not included here. This should now just be my code ready to merge.

--- index.js
+++ index.js
@@ -1,49 +1,4 @@
-import { dragElement, extractPickerHexValue } from './modules/utilities.js'
 
-const dragElementObj = document.getElementById("drag-object")
-const hexInput = document.getElementById('hexInput');
-const colorView = document.querySelector('.color-view');
-const swatchesContainer = document.querySelector('#swatches');
-
-const sliderPicker = new iro.ColorPicker("#sliderPicker", {
-    width: 200,
-    color: "rgb(255, 0, 0)",
-    borderWidth: 1,
-    borderColor: "#fff",
-    layout: [
-      {
-        component: iro.ui.Slider,
-        options: {
-          sliderType: 'hue'
-        }
-      },
-      {
-        component: iro.ui.Slider,
-        options: {
-          sliderType: 'saturation'
-        }
-      },
-      {
-        component: iro.ui.Slider,
-        options: {
-          sliderType: 'value'
-        }
-      },
-      ]
-    });
-
-sliderPicker.on(['color:init', 'color:change'], function(color){
-  const { hexString } = color
-  hexInput.value = hexString;
-  colorView.style.backgroundColor = hexString;
-  dragElementObj.style.backgroundColor = hexString;
-});
-
-hexInput.addEventListener('change', extractPickerHexValue);
-
-// drag and drop script
-
-dragElement(dragElementObj);
 
 
 // begin wendy's js
